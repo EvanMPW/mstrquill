e44a4b7c1bb0ae2018224e74e53b60fb599497a7
Evan Williams <EvanMPW@gmail.com>
2018-10-06 20:01:46 -0600

    Update quillall.js

--- quillall.js
+++ quillall.js
@@ -21,7 +21,7 @@ quill.on('text-change', function(delta, oldDelta, source) {
 
 function realSubmit() {
   document.querySelectorAll('[k="W49"]')[0].childNodes[0].click();
-  mstrmojo.all.mstr87.click();
+  document.querySelectorAll('[mstridx="0"]')[0].click();
 }
 
 //# sourceMappingURL=quill.min.js.map
